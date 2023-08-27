30bc953035478e60c67bf4235ceaa35dd3cbfc38
Ranco35 <44340061+Ranco35@users.noreply.github.com>
2023-08-27 11:02:51 -0400

    Delete whatsapp_connector_sale directory

--- whatsapp_connector_sale/models/SaleOrder.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields
-
-
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
-
-    conversation_id = fields.Many2one('acrux.chat.conversation', 'ChatRoom', ondelete='set null', copy=False)
